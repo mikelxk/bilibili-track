f94557a26faf593426bb3306b30f2a2a411ce05f
mikelxk <mike.lxk@gmail.com>
2021-02-12 01:22:25 +0800

    remove node script

--- README.md
+++ README.md
@@ -5,32 +5,14 @@
 ## Usage
 
 [Download deno](https://deno.land/)
+
 - directly call deno
-```shell
-deno run --allow-net main.ts -u $uuid -i $interval
-```
-- use node as shell commander
-```shell
-npm run start -u $uuid -i $interval
-```
-
-The interval defaults to 5s
-
-i.e
 
 ```shell
-npm start -- -u 777536
+./mod.ts -u $uuid -i $interval
 ```
-
-or directly
-
-```shell
-npm run lex
-```
-
-xD
 
 ## TODO:
 
-- add color
-- add more options in cli(i.e interval)
+- ~~add color~~
+- ~~add more options in cli(i.e interval)~~
--- mod.ts
+++ mod.ts
@@ -1,23 +1,17 @@
-import { parse } from "https://deno.land/std/flags/mod.ts";
-import { readLines } from "https://deno.land/std/io/bufio.ts";
-import { green, red, yellow } from "https://deno.land/std@0.86.0/fmt/colors.ts";
+#!/usr/bin/env -S deno run --unstable --allow-net --import-map=import_map.json
+import { parse } from "std/flags/mod.ts";
+import { readLines } from "std/io/bufio.ts";
+import { green, red, yellow } from "std/fmt/colors.ts";
 const args = parse(Deno.args);
-let uid = 2; //uncle's uid
-let interval = 5000;
+const uid = args.u || (await promptUid("Enter Uid:")) || 2; //uncle's uid
+const interval = args.i || 5000;//defaults to 5s
 async function promptUid(question: string) {
   console.log(question);
   for await (const line of readLines(Deno.stdin)) {
     return parseInt(line);
   }
 }
-if (args.u) {
-  uid = args.u;
-} else {
-  uid = (await promptUid("Enter Uid:")) || uid;
-}
-if (args.i) {
-  interval = args.i;
-}
+
 console.log(`Uid : ${uid}`);
 let beforeFollower = 0,
   afterFollower = 0;
@@ -25,7 +19,7 @@ setInterval(async () => {
   const res = await fetch(
     `https://api.bilibili.com/x/relation/stat?vmid=${uid}&jsonp=jsonp`,
   );
-  let data = await res.json();
+  const data = await res.json();
   let diffStr = "";
   let colorFunc = yellow;
   afterFollower = data.data.follower;
@@ -39,6 +33,9 @@ setInterval(async () => {
     colorFunc = red;
     diffStr = `${afterFollower - beforeFollower}`;
   }
-  console.log(`${new Date().toLocaleString()} : `,colorFunc(`${afterFollower} ${diffStr}`));
+  console.log(
+    `${new Date().toLocaleString()} : `,
+    colorFunc(`${afterFollower} ${diffStr}`),
+  );
   beforeFollower = afterFollower;
 }, interval);
