c1331d0267f78de1019ce091fc728256281a6205
Xiaokai Li <mike.lxk@gmail.com>
2021-02-11 10:13:18 +0800

    first draft

---
+++ index.mjs
@@ -0,0 +1,18 @@
+import { createInterface } from "readline";
+import axios from "axios";
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("up uid? :", async (uid) => {
+  await loopFunc(uid);
+});
+const loopFunc = async (uid, interval = 5000) => {
+  const apiUrl = `https://api.bilibili.com/x/relation/stat?vmid=${uid}&jsonp=jsonp`;
+  let prevFollower = 0;
+  setInterval(async () => {
+    let newFollower = await (await axios.get(apiUrl)).data.data.follower;
+    console.log(`Followers:${newFollower}, ${prevFollower < newFollower ? '+' : '-'}${Math.abs(newFollower - prevFollower)}`);
+    prevFollower = newFollower;
+  }, interval);
+};
