ff0ff245be589b1724be772f6a3de1d32c564076
mikelxk <mike.lxk@gmail.com>
2022-01-29 21:59:12 -0500

    feat: add nanami

--- scripts/nanami.ts
+++ scripts/nanami.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env -S deno run --allow-run
-const build = Deno.run({
+const nanami = Deno.run({
   cmd: [
     "deno",
-    "compile",
+    "run",
     "--allow-net",
-    "--allow-run",
-    "--output",
-    `bili-track`,
     "--import-map=import_map.json",
     "mod.ts",
+    "-u",
+    "434334701",
+    ...Deno.args,
   ],
 });
-await build.status();
+await nanami.status();
